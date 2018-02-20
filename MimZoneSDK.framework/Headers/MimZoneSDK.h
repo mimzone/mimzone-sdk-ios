4df22b8edef41f54334a8e9d1e62c0acd5d5b4a0
mimzone <no-reply@mim.zone>
2018-02-20 23:15:36 +0200

    Add SDK .framework

---
+++ MimZoneSDK.framework/Headers/MimZoneSDK.h
@@ -0,0 +1,21 @@
+//
+//  MimZoneSDK.h
+//  MimZoneSDK
+//
+//  Created by mim.zone Team on 02/02/2018.
+//  Copyright © 2018 mim.zone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MimZoneSDK.
+FOUNDATION_EXPORT double MimZoneSDKVersionNumber;
+
+//! Project version string for MimZoneSDK.
+FOUNDATION_EXPORT const unsigned char MimZoneSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MimZoneSDK/PublicHeader.h>
+
+
+#import "HashContext.h"
+#import "NSData+FastHex.h"
